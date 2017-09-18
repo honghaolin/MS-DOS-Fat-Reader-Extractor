e6ba0df0aabdbb836243adb105f42a06084066ff
Haolin Hong <hh443@cs.drexel.edu>
2017-09-18 19:45:59 -0400

    feat: add DirEntry

---
+++ src/DirEntry.c
@@ -0,0 +1,100 @@
+#include <string.h>
+#include "DirEntry.h"
+
+int loadDirEntry(DirEntry* dirEntry, FILE* file, int address) {
+    /* Seek to start address of dirEntry */
+    int reposition = fseek(file, address, SEEK_SET);
+    if (reposition) {
+        return -2;
+    }
+
+    /* load dirEntry */
+    size_t read = fread(dirEntry, sizeof(byte), 32, file);
+    if (read != 32) {
+        return -1;
+    }
+
+    /* Success */
+    return 0;
+}
+
+int isEmpty(const DirEntry* dirEntry) {
+    byte sign = dirEntry->name[0];
+    if (sign == 0x00 || sign == 0x20) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int isDeleted(const DirEntry* dirEntry) {
+    /* the DEL command will replace the first byte of the name
+       by 0xe5 to signify "deleted" */
+    byte sign = dirEntry->name[0];
+    if (sign == 0xE5 || sign == 0x05) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void getName(const DirEntry* dirEntry, char* name) {
+    memcpy(name, dirEntry->name, 8);
+    int i = 7;
+    while(i >= 0 && name[i] == ' ') {
+        i--;
+    }
+    name[i+1] = '\0';
+}
+
+void getExtension(const DirEntry* dirEntry, char* extension) {
+    memcpy(extension, dirEntry->extension, 3);
+    int i = 2;
+    while(i >= 0 && extension[i] == ' ') {
+        i--;
+    }
+    extension[i+1] = '\0';
+}
+
+int isVolumeLabel(const DirEntry* dirEntry) {
+    return (dirEntry->attribute[0] & 0x08) == 0x08;
+}
+
+int getHour(const DirEntry* dirEntry) {
+    return dirEntry->time[1] >> 3;
+}
+
+int getMinute(const DirEntry* dirEntry) {
+    int part1 = (0x07 & dirEntry->time[1]) << 3;
+    int part2 = dirEntry->time[0] >> 5;
+    return part1 + part2;
+}
+
+int getYear(const DirEntry* dirEntry) {
+    int year = dirEntry->date[1] >> 1;
+    return year + 1980;
+}
+
+int getMonth(const DirEntry* dirEntry) {
+    int part1 = (0x01 & dirEntry->date[1]) << 3;
+    int part2 = dirEntry->date[0] >> 5;
+    return part1 + part2;
+}
+
+int getDay(const DirEntry* dirEntry) {
+    return 0x1F & dirEntry->date[0];
+}
+
+int getStartCluster(const DirEntry* dirEntry) {
+    return (dirEntry->startCluster[1] << 8) + dirEntry->startCluster[0];
+}
+
+int getFileSize(const DirEntry* dirEntry) {
+    const byte* fileSize = dirEntry->fileSize;
+    int size = 0;
+    size += fileSize[0];
+    size += fileSize[1] << 8;
+    size += fileSize[2] << 16;
+    size += fileSize[3] << 24;
+    return size;
+}
---
+++ src/DirEntry.h
@@ -0,0 +1,52 @@
+/**
+ * DirEntry is the abbreviation for directory entry.
+ * It has 32 bytes.
+ *
+ * The format of this is:
+ * byte(s) contents
+ * ------- -------------------------------------------------------
+ * 0-10    File name (8 bytes) with extension (3 bytes)
+ * 11      Attribute - a bitvector. Bit 0: read only. Bit 1: hidden.
+ *         Bit 2: system file. Bit 3: volume label. Bit 4: subdirEntry.
+ *         Bit 5: archive. Bits 6-7: unused.
+ * 12-21   Reserved (see below)
+ * 22-23   Time (5/6/5 bits, for hour/minutes/doubleseconds)
+ * 24-25   Date (7/4/5 bits, for year-since-1980/month/day)
+ * 26-27   Starting cluster (0 for an empty file)
+ * 28-31   Filesize in bytes
+ */
+
+#ifndef _DIRENTRY_H_
+#define _DIRENTRY_H_
+
+#include <stdio.h>
+#include "ByteType.h"
+
+struct DirEntry {
+    byte name[8];           /* 00-07 */
+    byte extension[3];      /* 08-10 */
+    byte attribute[1];      /*  11   */
+    byte reserved[10];      /* 12-21 */
+    byte time[2];           /* 22-23 */
+    byte date[2];           /* 24-25 */
+    byte startCluster[2];   /* 26-27 */
+    byte fileSize[4];       /* 28-31 */
+};
+
+typedef struct DirEntry DirEntry;
+
+int loadDirEntry(DirEntry* dirEntry, FILE* file, int address);
+int isEmpty(const DirEntry* dirEntry);
+int isDeleted(const DirEntry* dirEntry);
+void getName(const DirEntry* dirEntry, char* name);
+void getExtension(const DirEntry* dirEntry, char* extension);
+int isVolumeLabel(const DirEntry* dirEntry);
+int getHour(const DirEntry* dirEntry);
+int getMinute(const DirEntry* dirEntry);
+int getYear(const DirEntry* dirEntry);
+int getMonth(const DirEntry* dirEntry);
+int getDay(const DirEntry* dirEntry);
+int getStartCluster(const DirEntry* dirEntry);
+int getFileSize(const DirEntry* dirEntry);
+
+#endif  /* _DIRENTRY_H_ */
